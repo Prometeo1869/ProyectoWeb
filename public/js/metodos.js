12d503753ef305d778007c22045390db1e35f2c7
Prometeo1869 <juancebrianp@gmail.com>
2022-04-08 14:37:37 +0200

    filtro

--- public/js/metodos.js
+++ public/js/metodos.js
@@ -1,7 +1,24 @@
-function desaparece() {
-    document.getElementById("portadaEventos").style.display = 'none';
+function soloSolteras() {
+    this.aparece();
+    document.getElementById('soltero').style.display = 'none';
+    document.getElementById('resto').style.display = 'none';
+    document.getElementById('vendedor').style.display = 'none';
 }
-function desaparece(id) {
-    let quitar = document.getElementById(id);
-    quitar.style.display = 'none';
+function soloSolteros() {
+    this.aparece();
+    document.getElementById('soltera').style.display = 'none';
+    document.getElementById('resto').style.display = 'none';
+    document.getElementById('vendedor').style.display = 'none';
+}
+function soloVendedores() {
+    this.aparece();
+    document.getElementById('soltero').style.display = 'none';
+    document.getElementById('resto').style.display = 'none';
+    document.getElementById('soltera').style.display = 'none';
+}
+function aparece() {
+    document.getElementById('soltera').style.display = 'flex';
+    document.getElementById('soltero').style.display = 'flex';
+    document.getElementById('resto').style.display = 'flex';
+    document.getElementById('vendedor').style.display = 'flex';
 }
