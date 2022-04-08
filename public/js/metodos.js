af35abfe679ba064bbf045b628dd87c022a4f3d9
Prometeo1869 <juancebrianp@gmail.com>
2022-04-08 13:04:01 +0200

    btnFiltroAlde

--- public/js/metodos.js
+++ public/js/metodos.js
@@ -1,3 +1,7 @@
 function desaparece() {
     document.getElementById("portadaEventos").style.display = 'none';
 }
+function desaparece(id) {
+    let quitar = document.getElementById(id);
+    quitar.style.display = 'none';
+}
