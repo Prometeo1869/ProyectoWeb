078fe96123b995eb47cfa57c1ee67a80e8b97661
Prometeo1869 <juancebrianp@gmail.com>
2022-04-09 12:06:00 +0200

    comentarMetodosJS

--- public/js/metodos.js
+++ public/js/metodos.js
@@ -1,14 +1,18 @@
+/*HACE DESAPARECER LA ETIQUETA AL PARAMETRO ID="id"*/
 function desaparece(id) {
     document.getElementById(id).style.display = 'none';
     
 }
-function aparece(id) {
+/*FILTRO DE <DIV class="card">, PRIMERO LOS QUITA TODOS Y LUEGO 
+PONE LOS QUE CORRESPONDAN AL PARAMETRO CLASS="clase"
+LAS VARIABLE x, y -> ARRAY CON CADA ELEMENTO*/
+function aparece(clase) {
     let x, y, i;
     x = document.getElementsByClassName('card');
     for(i = 0; i < x.length; i++){
         x[i].style.display = 'none';
     }    
-    y = document.getElementsByClassName(id);
+    y = document.getElementsByClassName(clase);
     for(i = 0; i < x.length; i++){
         y[i].style.display = 'flex';
     }
