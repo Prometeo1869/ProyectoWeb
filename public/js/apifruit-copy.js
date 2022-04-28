f201fed2a4b0e3b1cd5bdb4368f0b52da769ac8e
Prometeo1869 <juancebrianp@gmail.com>
2022-04-28 10:58:00 +0200

    prueba

---
+++ public/js/apifruit-copy.js
@@ -0,0 +1,32 @@
+
+let loading = document.getElementById('loading');
+let hexadecimal = document.getElementById('hexadecimal');
+let boton = document.getElementById('json_get');
+
+    boton.addEventListener('click', function() {
+        console.log(document.getElementById('red'));
+        if (document.getElementById('red').value == '') {
+            alert("Por favor, rellene el campos correctamente");
+        } else {
+            loading.style.display = 'block';
+            axios.get('https://www.fruityvice.com/api/fruit/' + document.getElementById('red').value,
+                {
+                    responseType: 'json'
+                })
+                .then(function (res) {
+                    if (res.status == 200) {
+                        console.log(res.data);
+                        document.getElementById('hexadecimal').value = res.data.genus;
+                    }
+                    console.log(res);
+                })
+                .catch(function (err) {
+
+                    console.log(err);
+                    document.getElementById('hexadecimal').value = "ERROR";
+                })
+                .then(function () {
+                    loading.style.display = 'none';
+                });
+        }
+    });
